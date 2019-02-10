7db4cb5f2e98169a99a1c0afb6616f43f95664b6
Serhii Kovalenko <serhii.kovalenko@otakoyi.com>
2019-02-11 01:24:34 +0200

    ggg

---
+++ index.js
@@ -0,0 +1,38 @@
+
+// Гра веселиця
+// var words = [
+//  "программа",
+//  "макака",
+//  "прекрасный",
+//  "оладушек"
+// ];
+
+// var word = words[Math.floor(Math.random() * words.length)];
+// var answerArray = [];
+//  for (var i = 0; i < word.length; i++) {
+//  answerArray[i] = "_";
+// }
+
+// var remainingLetters = word.length;
+
+// while(remainingLetters > 0) {
+// 	var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
+
+// 	if (guess === null) {
+// 		break;
+// 	} 
+// 	else if (guess.length !== 1) {
+// 		alert("Пожалуйста, введите только одну букву.");
+// 	} else { 
+
+// 		for (var j = 0; j < word.length; j++) {
+// 			if (word[j] === guess) {
+// 				answerArray[j] = guess;
+// 				remainingLetters--;
+// 			}
+// 		}
+// 		alert(answerArray);
+// 	}
+// }
+// alert(answerArray.join(" "));
+// alert("Отлично! Было загадано слово " + word);
